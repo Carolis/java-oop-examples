4861b851f391d1e588fd611eb1a4e7ed13b7bb03
Carolina Ale <redrumjedi@gmail.com>
2020-08-31 21:38:07 -0300

    add video1 example

---
+++ aula03/Video1/src/video1/Pen.java
@@ -0,0 +1,19 @@
+package video1;
+
+
+public class Pen {
+    String model;
+    String color;
+    float  nib;
+    int ink;
+    boolean capped;
+    
+    void seila() {
+    }
+
+    void cap() {
+    }
+
+    void uncap() {
+    }
+}
---
+++ aula03/Video1/src/video1/Video1.java
@@ -0,0 +1,15 @@
+
+package video1;
+
+
+public class Video1 {
+
+
+    public static void main(String[] args) {
+        Pen p1 = new Pen();
+        p1.color = "blue";
+        p1.nib = 0.5f;
+        p1.capped = false;
+    }
+    
+}
